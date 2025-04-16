1c9138317abf5f87091c43fe6e7cd75f24d70047
Nanda <nanda.page@tmdc.io>
2025-04-16 14:55:40 +0530

    Code added

---
+++ build/semantic-model/model/sqls/customer.sql
@@ -0,0 +1,9 @@
+SELECT 
+    customer_id, 
+    birth_year, 
+    education, 
+    marital_status, 
+    income, 
+    country 
+FROM 
+    icebase.sachin_exp.customer_data
---
+++ build/semantic-model/model/sqls/product.sql
@@ -0,0 +1,8 @@
+select 
+    customer_id as product_customer_id,
+    product_id,
+    product_category,
+    product_name,
+    price
+FROM
+    icebase.sachin_exp.product_data
